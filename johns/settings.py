c1730d5ade68abcdcca01f81d9933c1916822036
pramod <gopinath.pramod@gmail.com>
2024-09-26 23:44:39 +0530

    aws

--- johns/settings.py
+++ johns/settings.py
@@ -142,3 +142,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_KEY")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S_KEY")
+AWS_STORAGE_BUCKET_NAME = 'johnsconcept'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
